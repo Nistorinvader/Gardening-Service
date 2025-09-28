8d4f5598ae8da32c4a9f34a8cb6853a73d96ad89
Dmytro Hensirovskyi <120091504+DHensirovskyi@users.noreply.github.com>
2025-09-28 23:58:53 +0200

    Update ProjectsCarousel.tsx

--- src/app/MainPage Components/Gallery/ProjectsCarousel.tsx
+++ src/app/MainPage Components/Gallery/ProjectsCarousel.tsx
@@ -12,49 +12,56 @@ type ProjectsCarouselProps = {
   type: true | false; 
 };
 
-
-
 export default function ProjectsCarousel({ image, video, type, poster }: ProjectsCarouselProps) {
+  const validImages = (image || []).filter(img => img && img.fields && img.fields.file && img.fields.file.url);
+  const validVideos = (video || []).filter(vid => vid && vid.fields && vid.fields.file && vid.fields.file.url);
+  const validPosters = (poster || []).filter(p => p && p.fields && p.fields.file && p.fields.file.url);
 
-  const imageSlides = image.map((img) => (
-    <Carousel.Slide key={`https:${img.fields.file.url}`} className={classes.card}>
+  const imageSlides = validImages.map((img, index) => (
+    <Carousel.Slide key={`img-${index}`} className={classes.card}>
       <Image 
         src={`https:${img.fields.file.url}`}
         width={302}
         height={389} 
-        alt={`https:${img.fields.file.url}`} 
+        alt={`Gallery image ${index + 1}`} 
         className={classes.object}
       />
     </Carousel.Slide>
   ));
 
-  const videoSlides = video.map((vid, index) => (
-  <Carousel.Slide key={`https:${vid.fields.file.url}`} className={classes.card}>
-    <VideoPlayer 
-      videoSrc={`https:${vid.fields.file.url}`} 
-      posterSrc={poster && poster[index] ? `https:${poster[index].fields.file.url}` : ''} 
-    />
-  </Carousel.Slide>
-));
+  const videoSlides = validVideos.map((vid, index) => (
+    <Carousel.Slide key={`vid-${index}`} className={classes.card}>
+      <VideoPlayer 
+        videoSrc={`https:${vid.fields.file.url}`} 
+        posterSrc={validPosters[index] ? `https:${validPosters[index].fields.file.url}` : ''} 
+      />
+    </Carousel.Slide>
+  ));
+
+  const slides = type ? imageSlides : videoSlides;
+  
+  if (slides.length === 0) {
+    return null;
+  }
 
   return (
     <section style={{padding: '16px 0'}}>
-    <Carousel
-      withIndicators
-      height={389}
-      slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
-      slideGap="sm"
-      withControls
-      emblaOptions={{ loop: true, align: 'start', slidesToScroll: 1 }}
-      styles={{
-        control: {
-          backgroundColor: 'black',
-          color: 'white'
-        },
-      }}
-    >
-      {type ? imageSlides : videoSlides}
-    </Carousel>
+      <Carousel
+        withIndicators
+        height={389}
+        slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
+        slideGap="sm"
+        withControls
+        emblaOptions={{ loop: true, align: 'start', slidesToScroll: 1 }}
+        styles={{
+          control: {
+            backgroundColor: 'black',
+            color: 'white'
+          },
+        }}
+      >
+        {slides}
+      </Carousel>
     </section>
   );
 }
